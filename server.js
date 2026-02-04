07564be3c13a3aa37538cf35061b516fbb90f924
buithiquyentran <buithiquyentran29112003@gmail.com>
2026-02-04 17:03:48 +0700

    feat: initialize Express application with basic setup - Add package.json with dependencies for express, morgan, helmet, and compression - Create server.js to start the Express server and handle shutdown gracefully

---
+++ server.js
@@ -0,0 +1,12 @@
+import app from "./src/app.js";
+const PORT = process.env.PORT || 3000;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+})
+
+process.on('SIGINT', () => {
+  server.close(() => {
+    console.log('Server closed');
+    process.exit(0);
+  });
+});
---
+++ src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import compression from "compression";
+const app = express();
+// init middlewares
+app.use(morgan("dev"));
+// app.use(morgan("combined"));
+// app.use(morgan("common"));
+// app.use(morgan("short"));
+// app.use(morgan("tiny"));
+
+app.use(helmet());
+// app.use(compression());
+//init db
+
+// init routes
+app.get("/", (req, res, next) => {
+  const strCompress = "tran";
+  return res
+    .status(200)
+    .json({ message: "API is working!", metadata: strCompress.repeat(1000) });
+});
+// handle error
+
+export default app;
