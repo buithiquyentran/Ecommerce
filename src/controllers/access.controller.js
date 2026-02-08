ea21e3ccec242266194e94a599ee18802028ea83
buithiquyentran <buithiquyentran29112003@gmail.com>
2026-02-08 11:18:09 +0700

    feat: refactor signUp method to use success response classes

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,8 +1,15 @@
 import { accessService } from "../services/access.service.js";
+import { OK, Created } from "../core/success.response.js";
 class AccessController {
   // Add methods for access control here
   async signUp(req, res, next) {
-      return res.status(201).json(await accessService.signUp(req.body));
+    new Created({
+      message: "User created successfully",
+      metadata: await accessService.signUp(req.body),
+      options: { limit: 10 },
+    }).send(res);
+
+    // return res.status(201).json(await accessService.signUp(req.body));
   }
 }
 export const accessController = new AccessController();
